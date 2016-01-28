1772a62821bdf66f46855290326c8a9d995583b3
neilzhuo <zhuoyc@126.com>
2016-01-28 15:20:27 +0800

    implement a private video player view.

---
+++ app/src/main/java/com/neilzhuo/ijkplayerdemo/PlayerActivity.java
@@ -0,0 +1,170 @@
+package com.neilzhuo.ijkplayerdemo;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
+import tv.danmaku.ijk.media.player.misc.ITrackInfo;
+import tv.danmaku.ijk.media.sample.application.Settings;
+import tv.danmaku.ijk.media.sample.content.RecentMediaStorage;
+import tv.danmaku.ijk.media.sample.fragments.TracksFragment;
+import tv.danmaku.ijk.media.sample.widget.media.AndroidMediaController;
+import tv.danmaku.ijk.media.sample.widget.media.IjkVideoView;
+import tv.danmaku.ijk.media.sample.widget.media.MeasureHelper;
+
+public class PlayerActivity extends AppCompatActivity implements TracksFragment.ITrackHolder {
+    private static final String TAG = "PlayerActivity";
+
+    private String mVideoPath;
+    private Uri    mVideoUri;
+
+    private AndroidMediaController mMediaController;
+    private IjkVideoView mVideoView;
+    private TextView mTextView;
+    private boolean mBackPressed;
+
+    public static Intent newIntent(Context context, String videoPath, String videoTitle) {
+        Intent intent = new Intent(context, PlayerActivity.class);
+        intent.putExtra("videoPath", videoPath);
+        intent.putExtra("videoTitle", videoTitle);
+        return intent;
+    }
+
+    public static void intentTo(Context context, String videoPath, String videoTitle) {
+        context.startActivity(newIntent(context, videoPath, videoTitle));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.neilzhuo.ijkplayerdemo.R.layout.activity_private_player);
+
+        // handle arguments
+        mVideoPath = getIntent().getStringExtra("videoPath");
+
+        Intent intent = getIntent();
+        String intentAction = intent.getAction();
+        if (!TextUtils.isEmpty(intentAction)) {
+            if (intentAction.equals(Intent.ACTION_VIEW)) {
+                mVideoPath = intent.getDataString();
+            } else if (intentAction.equals(Intent.ACTION_SEND)) {
+                mVideoUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                    String scheme = mVideoUri.getScheme();
+                    if (TextUtils.isEmpty(scheme)) {
+                        Log.e(TAG, "Null unknown ccheme\n");
+                        finish();
+                        return;
+                    }
+                    if (scheme.equals(ContentResolver.SCHEME_ANDROID_RESOURCE)) {
+                        mVideoPath = mVideoUri.getPath();
+                    } else if (scheme.equals(ContentResolver.SCHEME_CONTENT)) {
+                        Log.e(TAG, "Can not resolve content below Android-ICS\n");
+                        finish();
+                        return;
+                    } else {
+                        Log.e(TAG, "Unknown scheme " + scheme + "\n");
+                        finish();
+                        return;
+                    }
+                }
+            }
+        }
+
+        // init UI
+
+        mMediaController = new AndroidMediaController(this, false);
+
+        // init player
+        IjkMediaPlayer.loadLibrariesOnce(null);
+        IjkMediaPlayer.native_profileBegin("libijkplayer.so");
+
+        mVideoView = (IjkVideoView) findViewById(R.id.private_video_view);
+        mVideoView.setMediaController(mMediaController);
+        // prefer mVideoPath
+        if (mVideoPath != null)
+            mVideoView.setVideoPath(mVideoPath);
+        else if (mVideoUri != null)
+            mVideoView.setVideoURI(mVideoUri);
+        else {
+            Log.e(TAG, "Null Data Source\n");
+            finish();
+            return;
+        }
+        mVideoView.start();
+
+        // add some text under video view
+
+        mTextView = (TextView)findViewById(R.id.private_text_view);
+        mTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        mTextView.setText("仔细看完这个视频，然后回答以下问题：\n");
+        mTextView.append("1. 猫被抓住了吗?");
+    }
+
+    @Override
+    public void onBackPressed() {
+        mBackPressed = true;
+
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mBackPressed || !mVideoView.isBackgroundPlayEnabled()) {
+            mVideoView.stopPlayback();
+            mVideoView.release(true);
+            mVideoView.stopBackgroundPlay();
+        } else {
+            mVideoView.enterBackground();
+        }
+        IjkMediaPlayer.native_profileEnd();
+    }
+
+    @Override
+    public ITrackInfo[] getTrackInfo() {
+        if (mVideoView == null)
+            return null;
+
+        return mVideoView.getTrackInfo();
+    }
+
+    @Override
+    public void selectTrack(int stream) {
+        mVideoView.selectTrack(stream);
+    }
+
+    @Override
+    public void deselectTrack(int stream) {
+        mVideoView.deselectTrack(stream);
+    }
+
+    @Override
+    public int getSelectedTrack(int trackType) {
+        if (mVideoView == null)
+            return -1;
+
+        return mVideoView.getSelectedTrack(trackType);
+    }
+}
